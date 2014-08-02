040d4f0a369fb07c9443cab8db66cbed35f03b7f
fluffy4eva <michaelrybar@gmail.com>
2014-08-02 16:28:21 -0400

    deleted chatpackethandler

--- src/chat/packetHandlers/ChatMessagePacketHandler.java
+++ src/chat/packetHandlers/ChatMessagePacketHandler.java
@@ -7,6 +7,12 @@ import main.Client;
 import main.PacketHandler;
 import main.logging.Logger;
 
+
+/**
+ * This receives all packets that are chat message packets and creates a handler 
+ * @author Michael
+ *
+ */
 public class ChatMessagePacketHandler extends PacketHandler {
 
   public ChatMessagePacketHandler(Packet packet, Client client) {
@@ -16,7 +22,7 @@ public class ChatMessagePacketHandler extends PacketHandler {
   @Override
   public Packet handle() {
 
-    Logger.log(packet.toString(), Logger.INFO);
+    Logger.info(packet.toString());
 
     ChatMessageHandler handler = new ChatMessageHandler((ChatMessage) packet);
     return handler.handleMessage(client.getClientId());
